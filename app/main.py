9a15eddcf92683d69712667a56f114192728cf35
Taha <taha.m4831@gmail.com>
2026-02-09 21:29:50 +0330

    create and complete main.py file

---
+++ app/main.py
@@ -0,0 +1,32 @@
+import sys
+from summarizer import mmr_select, load_data
+from postprocess import clean_summary
+
+def main():
+
+    #Load sentences & embeddings 
+    try:
+        sents, embs = load_data()
+    except FileNotFoundError:
+        print("Error: sentences.json or embedded.npy not found.")
+        sys.exit(1)
+
+    # Choose important sentences with MMR
+    summary, idxs, scores = mmr_select(embs, sents, summary_size=5, lambda_param=0.7)
+
+    # Post-process
+    summary_clean = clean_summary(summary)
+
+    # Output on terminal
+    print("\n=== SUMMARY ===\n")
+    print(summary_clean)
+    print("\nSelected sentence indices:", idxs)
+    print("Relevance scores:", scores)
+
+    # save summary
+    with open("app/summary.txt", "w", encoding="utf-8") as f:
+        f.write(summary_clean)
+    print("\nSaved summary -> app/summary.txt")
+
+if __name__ == "__main__":
+    main()
